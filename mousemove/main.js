1e722655acca64a31c0e140d4e5a51a71f9900d5
AbhishekBhanot5991 <abhishekbhanot5991@gmail.com>
2023-09-16 12:10:26 +0530

    initial commit

--- mousemove/main.js
+++ mousemove/main.js
@@ -10,9 +10,25 @@ rect.addEventListener("mousemove",(details)=>{
     // insideRectVal = Math.round(insideRectVal * 100) / 100;
     console.log(insideRectVal)
     if(insideRectVal < rectangleLocation.width/2){
-        console.log('left side')
+        // console.log('left side')
+        var redColor = gsap.utils.mapRange(0, rectangleLocation.width/2, 255, 0, insideRectVal)
+        gsap.to(rect, {
+            backgroundColor: `rgb(${redColor}, 0, 0)`,
+            ease:Power4
+        })
     }
     else{
-        console.log('right side')
+        // console.log('right side')
+        var blueColor = gsap.utils.mapRange(rectangleLocation.width/2, rectangleLocation.width, 0, 255, insideRectVal)
+        gsap.to(rect, {
+            backgroundColor: `rgb(0, 0, ${blueColor})`,
+            ease:Power4
+        })
     }
 })
+rect.addEventListener("mouseleave",()=>{
+    gsap.to(rect, {
+        backgroundColor:"#fff"
+    })
+})
+// console.log()
